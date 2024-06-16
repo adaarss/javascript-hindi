b6a481fa04e1de388c86a1be28db4bb5d280f5db
Adarsh Nallamothu <112814013+adaarss@users.noreply.github.com>
2024-06-16 21:56:24 +0000

    datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,12 @@
+'use strict' // treat all javascript code into newer version
+
+//alert('Hello') we are using nodejs not browser
+let name='Adarsh'//string
+let Age =24 //number
+let isloggedin= false //boollean
+
+// null => standlone type
+// undefined => the value is not defined
+
+console.log(typeof undefined ) //type is undefined
+console.log(typeof null) // type is object
---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+const accountId =14434
+let accountEmail = 'adarsh@gmail.com'
+var accountPassword = '12345';
+let accountState
+accountCity='Jaipur'
+
+
+accountEmail='suresh@gmail.com';
+accountPassword='21345';
+accountState ='Texas'
+//prefer not use var
+/* prefer not use Var
+because of issue in functional scope and block scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
