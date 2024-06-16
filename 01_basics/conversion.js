b0fc5fb1e3095e6109eff5fdac41499a172009f6
Adarsh Nallamothu <112814013+adaarss@users.noreply.github.com>
2024-06-16 22:43:40 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,15 @@
+let age =undefined
+//console.log(typeof age);
+//console.log(typeof(age));
+
+
+let valueInNumber= Number(age);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+let isLoggedIn=0
+
+let isBoolean= Boolean(isLoggedIn)
+
+console.log(typeof isBoolean);
+console.log(isBoolean)
